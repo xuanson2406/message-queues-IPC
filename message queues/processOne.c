fd8e4a7fa07270f21537b90c42f6b735410201b4
sondx <son.daoxuan2406@gmail.com>
2022-11-20 23:11:56 +0700

    IPC practice

--- message queues/processOne.c
+++ message queues/processOne.c
@@ -3,6 +3,8 @@
 #include <sys/msg.h>
 #include <stdio.h>
 #include <string.h>
+#include<unistd.h>
+#include<signal.h>
 #define MSGSZ     128
 
 
@@ -15,8 +17,11 @@ typedef struct msgbuf {
 	char    mtext[MSGSZ];
 } message_buf;
 
-main()
+void handleSignals(int signum);
+
+int main(void)
 {
+	
 	int msqid;
 	int msgflg = IPC_CREAT | 0666;
 	key_t key;
@@ -24,47 +29,77 @@ main()
 	size_t buf_length;
 	key = 1234;
 
-	if ((msqid = msgget(key, msgflg )) < 0) {
-		perror("msgget");
-		exit(1);
-	}
+	
 	
 	while(1){
-		
-		sbuf.mtype = 1;
-		printf("\nEnter Message '***' to close connection:");
-		gets(&sbuf.mtext);
-		
-		if(strcmp(sbuf.mtext,"***")==0){
-			strcpy(sbuf.mtext,"\nConnection closed by Other User");
-			buf_length = strlen(sbuf.mtext) + 1 ;
-			if (msgsnd(msqid, &sbuf, buf_length, IPC_NOWAIT) < 0) {
-				printf ("%d, %d, %s, %d\n", msqid, sbuf.mtype, sbuf.mtext, buf_length);
-				perror("msgsnd");
-				exit(1);
-			}
-			printf("\nClosing Connection");
-			break;
+		if ((msqid = msgget(key, msgflg )) < 0) {
+		perror("msgget");
+		exit(1);
 		}
-		
-		
-		
-		buf_length = strlen(sbuf.mtext) + 1 ;
-		if (msgsnd(msqid, &sbuf, buf_length, IPC_NOWAIT) < 0) {
-			printf ("%d, %d, %s, %d\n", msqid, sbuf.mtype, sbuf.mtext, buf_length);
-			perror("msgsnd");
-			exit(1);
-		}else 
-			printf("\nProcess 1:%s", sbuf.mtext);
-	
+		void (*sigHandlerInterrupt)(int);
+	   	void (*sigHandlerQuit)(int);
+	   	void (*sigHandlerReturn)(int);
+	   	sigHandlerInterrupt = sigHandlerQuit = handleSignals;
+	   	sigHandlerReturn = signal(SIGINT, sigHandlerInterrupt);
+	   	if (sigHandlerReturn == SIG_ERR) {
+	      		perror("signal error: ");
+	      		return 1;
+	   	}
+	   	sigHandlerReturn = signal(SIGQUIT, sigHandlerQuit);
+	   
+	   	if (sigHandlerReturn == SIG_ERR) {
+	      		perror("signal error: ");
+	      		return 1;
+	   	}		
 		if (msgrcv(msqid, &rbuf, MSGSZ, 2, 0) < 0) {
 			perror("msgrcv");
+			//msgctl(msqid, IPC_RMID, NULL);
 			exit(1);
 		}else
-			printf("\nProcess 2:%s", rbuf.mtext);
+			printf("\nProcess 2:%s\n", rbuf.mtext);
 	}
-	
-	
 
 	exit(0);
+}
+void handleSignals(int signum) {
+   int msqid;
+   message_buf sbuf;
+   int msgflg = IPC_CREAT | 0666;
+   size_t buf_length;
+   sbuf.mtype = 1;
+   key_t key;
+   key = 1234;
+   if ((msqid = msgget(key, msgflg )) < 0) {
+		perror("msgget");
+		exit(1);
+   }
+   switch(signum) {
+      case SIGINT:
+      
+      strcpy(sbuf.mtext,"Received signal CTRL+C from process 1");
+      buf_length = strlen(sbuf.mtext) + 1 ;
+      if (msgsnd(msqid, &sbuf, buf_length, IPC_NOWAIT) < 0) {
+      		printf ("%d, %d, %s, %d\n", msqid, sbuf.mtype, sbuf.mtext, buf_length);
+		perror("msgsnd");
+		exit(1);
+      }else
+		printf("\nProcess 1: CTRL+C \n");
+      signal(SIGINT, SIG_DFL);
+      break;
+      case SIGQUIT:
+      
+      strcpy(sbuf.mtext,"Received signal CTRL+\\ from process 1");
+      buf_length = strlen(sbuf.mtext) + 1 ;
+      if (msgsnd(msqid, &sbuf, buf_length, IPC_NOWAIT) < 0) {
+      		printf ("%d, %d, %s, %d\n", msqid, sbuf.mtype, sbuf.mtext, buf_length);
+		perror("msgsnd");
+		exit(1);
+      }else
+		printf("\nProcess 1: CTRL+\\ \n");
+      break;
+      default:
+      printf("\nReceived signal number %d\n", signum);
+      break;
+   }
+   return;
 }
--- message queues/processTwo.c
+++ message queues/processTwo.c
@@ -3,6 +3,8 @@
 #include <sys/msg.h>
 #include <stdio.h>
 #include <string.h>
+#include<unistd.h>
+#include<signal.h>
 #define MSGSZ     128
 
 
@@ -17,9 +19,10 @@ typedef struct msgbuf {
 	char    mtext[MSGSZ];
 } message_buf;
 
-
-main()
+void handleSignals(int signum);
+int main(void)
 {
+        
 	int msqid;
 	key_t key;
 	message_buf  rbuf,sbuf;
@@ -31,47 +34,79 @@ main()
 	*/
 	key = 1234;
 
-	if ((msqid = msgget(key, 0666)) < 0) {
-		perror("msgget");
-		exit(1);
-	}
+	
 
 
 	while(1){
-		
+		if ((msqid = msgget(key, 0666)) < 0) {
+		perror("msgget");
+		exit(1);
+		}
+		void (*sigHandlerInterrupt)(int);
+	   	void (*sigHandlerQuit)(int);
+	   	void (*sigHandlerReturn)(int);
+	   	sigHandlerInterrupt = sigHandlerQuit = handleSignals;
+	   	sigHandlerReturn = signal(SIGINT, sigHandlerInterrupt);
+	   	if (sigHandlerReturn == SIG_ERR) {
+	      		perror("signal error: ");
+	      		return 1;
+	   	}
+	   	sigHandlerReturn = signal(SIGQUIT, sigHandlerQuit);
+	   
+	   	if (sigHandlerReturn == SIG_ERR) {
+	      		perror("signal error: ");
+	      		return 1;
+	   	}
+
+
 		if (msgrcv(msqid, &rbuf, MSGSZ, 1, 0) < 0) {
 			perror("msgrcv");
+			//msgctl(msqid, IPC_RMID, NULL);
 			exit(1);
 		}else
-			printf("\nProcess 1:%s", rbuf.mtext);
-		
-		
-		
-		sbuf.mtype = 2;
-		printf("\nEnter Message '***' to close connection:");
-		gets(&sbuf.mtext);
-		if(strcmp(sbuf.mtext,"***")==0){
-			strcpy(sbuf.mtext,"Connection closed by Other Process");
-			buf_length = strlen(sbuf.mtext) + 1 ;
-			if (msgsnd(msqid, &sbuf, buf_length, IPC_NOWAIT) < 0) {
-				printf ("%d, %d, %s, %d\n", msqid, sbuf.mtype, sbuf.mtext, buf_length);
-				perror("msgsnd");
-				exit(1);
-			}
-			printf("\nClosing Connection");
-			break;
-		}
-			
-		buf_length = strlen(sbuf.mtext) + 1 ;
-		if (msgsnd(msqid, &sbuf, buf_length, IPC_NOWAIT) < 0) {
-			printf ("%d, %d, %s, %d\n", msqid, sbuf.mtype, sbuf.mtext, buf_length);
-			perror("msgsnd");
-			exit(1);
-		}else 
-			printf("Process 2:%s", sbuf.mtext);
-	
-
+			printf("\nProcess 1:%s\n", rbuf.mtext);	
 	}
 	
 	exit(0);
+}
+void handleSignals(int signum) {
+   int msqid;
+   message_buf sbuf;
+   size_t buf_length;
+   sbuf.mtype = 2;
+   key_t key;
+   key = 1234;
+   if ((msqid = msgget(key, 0666 )) < 0) {
+		perror("msgget");
+		exit(1);
+   }
+   switch(signum) {
+      case SIGINT:
+      strcpy(sbuf.mtext,"Received signal CTRL+C from process 2");
+      buf_length = strlen(sbuf.mtext) + 1 ;
+      if (msgsnd(msqid, &sbuf, buf_length, IPC_NOWAIT) < 0) {
+      		printf ("%d, %d, %s, %d\n", msqid, sbuf.mtype, sbuf.mtext, buf_length);
+		perror("msgsnd");
+		exit(1);
+      }else 
+		printf("\nProcess 2: CTRL+C \n");
+      signal(SIGINT, SIG_DFL);
+      
+      break;
+      case SIGQUIT:
+      
+      strcpy(sbuf.mtext,"Received signal CTRL+\\ from process 2");
+      buf_length = strlen(sbuf.mtext) + 1 ;
+      if (msgsnd(msqid, &sbuf, buf_length, IPC_NOWAIT) < 0) {
+      		printf ("%d, %d, %s, %d\n", msqid, sbuf.mtype, sbuf.mtext, buf_length);
+		perror("msgsnd");
+		exit(1);
+      }else
+		printf("\nProcess 2: CTRL+\\ \n");
+      break;
+      default:
+      printf("\nReceived signal number %d\n", signum);
+      break;
+   }
+   return;
 }
